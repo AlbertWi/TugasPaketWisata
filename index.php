7be85a4baf35385f0ec3eaa998369f0a96604ade
EricJonathan <cesyacatherine_2226240018@mhs.mdp.ac.id>
2024-09-25 01:40:29 +0700

    updatre
    update

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ $destinations = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
     <title>WanderLao - Discover China's Wonders</title>
+
     
 </head>
 <body>
