51995b214bc0e968790fc48c0a08d9a5d4c04ca6
AlbertWi <albertwijaya_2226240029@mhs.mdp.ac.id>
2024-09-25 00:35:04 +0700

    Updatre Summer Season
    Tambahi Musim panas

--- homepage.php
+++ homepage.php
@@ -12,6 +12,27 @@
     </header>
     <section class="season-list">
     <div class="season-section">
+    <div class="season-section">
+            <h2>Summer Season</h2>
+            <div class="paket-list">
+                <div class="paket-item">
+                <img src="https://cdn.idntimes.com/content-images/community/2023/12/ralf-leineweber-srmzadjwk-e-unsplash-5e62e4610f3cf60b11909aee76ddca6d-6720e51ebad83495912f919720fe1a5a_600x400.jpg" alt="Wisata Beijing - Musim Semi" class="paket-gambar" width="240px">
+                    <h3>Shanghai Trip - Summer</h3>
+                    <p>Visit the Bund and the Shanghai tower in sunny summer weather.</p>
+                    <p>Price: IDR 7.000.000</p>
+                    <p>Duration: 7 days</p>
+                    <a href="booking.php?id=3&season=Musim%20Panas" class="btn">Book Now</a>
+                </div>
+                <div class="paket-item">
+                <img src="https://www.callistatour.com/data/img/tulisan/pesona-hainan-2024-petualangan-tropis-yang-tak-terlupakan.jpg" alt="Wisata Beijing - Musim Semi" class="paket-gambar" width="240px">
+                    <h3>Hainan Trip - Summer</h3>
+                    <p>A tropical getaway on the exotic and warm island of Hainan.</p>
+                    <p>Price: IDR 7,500,000</p>
+                    <p>Duration: 6 days</p>
+                    <a href="booking.php?id=4&season=Musim%20Panas" class="btn">Book Now</a>
+                </div>
+            </div>
+        </div>
             <h2>Spring Season</h2>
             <div class="paket-list">
                 <div class="paket-item">
