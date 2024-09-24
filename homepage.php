30e9c6cc6345a3edc57ff829a75fed2a4fd32509
AlbertWi <albertwijaya_2226240029@mhs.mdp.ac.id>
2024-09-25 00:30:35 +0700

    Buat Spring Season
    buat musim gugur

--- homepage.php
+++ homepage.php
@@ -10,5 +10,29 @@
     <header>
         <h1>China Tour Packages - By Season</h1>
     </header>
+    <section class="season-list">
+    <div class="season-section">
+            <h2>Spring Season</h2>
+            <div class="paket-list">
+                <div class="paket-item">
+                    <img src="https://img.caixin.com/2022-05-20/165304582100894_840_560.jpg" alt="Wisata Beijing - Musim Semi" class="paket-gambar" width="240px">
+                     <h3>Beijing Trip - Spring</h3>
+                     <p>Enjoy cherry blossoms and visit the Great Wall of China in spring.</p>
+                     <p>Price: IDR 5,500,000</p>
+                     <p>Duration: 5 days</p>
+                     <a href="booking.php?id=1&season=Musim%20Semi" class="btn">Book Now</a>
+            </div>
+
+                <div class="paket-item">
+                    <img src="https://cdn.idntimes.com/content-images/community/2021/04/img-20210429-070500-9f2690fd965b0b529c7039fa544e675e-7017fb9372d748f2d0543022e6ca3a9d.jpg" alt="Wisata Beijing - Musim Semi" class="paket-gambar" width="240px">
+                    <h3>Hangzhou Trip - Spring</h3>
+                    <p>Beautiful West Lake, surrounded by blooming flowers.</p>
+                    <p>Price: IDR 6,000,000</p>
+                    <p>Duration: 5 days</p>
+                    <a href="booking.php?id=2&season=Musim%20Semi" class="btn">Book Now</a>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
